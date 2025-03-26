00a9c592f1497f31d52045010741fcd0a03273c0
Aquamarine Hoshino <cozoral@outlook.com>
2025-03-26 22:12:13 +0100

    Add project files.

---
+++ Tatsunami/Program.cs
@@ -0,0 +1,38 @@
+using MudBlazor;
+using MudBlazor.Services;
+using Tatsunami.Components;
+
+namespace Tatsunami;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+
+        builder.Services.AddMudServices();
+        builder.Services.AddMudMarkdownServices();
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
